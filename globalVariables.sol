e0c3c89ca8f984a16dd854a821886ac853e5770a
Nworah_Gabriel <gabrielnworah6@gmail.com>
2025-04-11 17:40:36 +0800

    Created and added the global variable file to index

---
+++ globalVariables.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+
+contract GlobalVariables{
+    
+    function GetInteractorAddress() public view returns (address) {
+        return msg.sender;
+    }
+
+    function GetGas() public view returns (uint) {
+        return gasleft();
+    }
+    function GetTimeStamp() public view returns (uint) {
+        return block.timestamp;
+    }
+    
+    function GetEth() public payable returns (uint) {
+        return msg.value;
+    }
+    
+    function GetChainId() public payable returns (uint) {
+        return block.chainid;
+    }
+}
